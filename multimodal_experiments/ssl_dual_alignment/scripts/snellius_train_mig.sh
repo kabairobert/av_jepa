6fa03f9eaa209cbcd216ec635dfd02ca4a152eff
kabairobert <kabairobert@gmail.com>
2026-05-09 21:05:29 +0200

    Update snellius_train_mig script to adjust job time and enhance logging notes format

--- multimodal_experiments/ssl_dual_alignment/scripts/snellius_train_mig.sh
+++ multimodal_experiments/ssl_dual_alignment/scripts/snellius_train_mig.sh
@@ -6,7 +6,7 @@
 #SBATCH --cpus-per-task=9
 #SBATCH --gpus=1
 #SBATCH --mem=60G
-#SBATCH --time=05:00:00
+#SBATCH --time=01:30:00
 #SBATCH --output=%x_%j.out
 #SBATCH --error=%x_%j.err
 
@@ -43,7 +43,7 @@ total=${#CFG_MATRIX[@]}
 idx=1
 for cfg in "${CFG_MATRIX[@]}"; do
     echo "Starting training ${idx}/${total}: ${cfg}"
-    uv run python -m multimodal_experiments.ssl_dual_alignment.main --fname "${cfg}" --logging.notes "SSL dual alignment w/noise, 5 model vers"
+    uv run python -m multimodal_experiments.ssl_dual_alignment.main --fname "${cfg}" --logging.notes "SSL_align_noise|data:3D2f-rot|pred:mlp/diag|l1:true/false|wsprase:0/0.1"
     idx=$((idx + 1))
 done
 
